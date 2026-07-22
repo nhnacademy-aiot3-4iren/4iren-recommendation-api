0bb18aaceb6261a5f1bd385b059825bd549f0300
yooncy123 <nightbang21@gmail.com>
2026-07-22 10:55:02 +0900

    fix: FeigncClient 수정
    Fixes #30

--- src/main/java/com/nhnacademy/recommendation/adaptor/CoreClient.java
+++ src/main/java/com/nhnacademy/recommendation/adaptor/CoreClient.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "${core-service}")
+@FeignClient(name = "4iren-core")
 public interface CoreClient {
 
     @GetMapping("/api/kma/ultraSrtNcst")
